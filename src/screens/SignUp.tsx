a3a775c7f87addf7205323606fd67cd07f86c92c
glaubermatos <glaub.oliveira@hotmail.com>
2023-08-11 00:21:52 -0300

    api removida - tratamento de erro com axios interceptors

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Center, Text, Heading, ScrollView } from 'native-base'
+import { VStack, Image, Center, Text, Heading, ScrollView, useToast } from 'native-base'
 import { useForm, Controller } from "react-hook-form"
 import * as yup from "yup"
 import { yupResolver } from "@hookform/resolvers/yup"
@@ -7,8 +7,11 @@ import { yupResolver } from "@hookform/resolvers/yup"
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png';
 
+import { AppError } from '@utils/AppError';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { api } from '@services/api';
 
 
 
@@ -29,20 +32,34 @@ type FormDataProps = yup.InferType<typeof SignUpSchema>;
 
 export function SignUp() {
     const navigation = useNavigation()
+    const toast = useToast()
 
-    const { control, handleSubmit, formState: {errors}} = useForm<FormDataProps>({
+    const { control, handleSubmit, reset, formState: {errors}} = useForm<FormDataProps>({
       resolver: yupResolver(SignUpSchema),
-
-      // defaultValues: {
-      //   name: "",
-      //   email: "",
-      //   password: "",
-      //   password_confirm: ""
-      // }
+      defaultValues: {
+        name: "",
+        email: "",
+        password: "",
+        password_confirm: ""
+      }
     })
 
-    function handleSignUp(data: FormDataProps) {
-      console.log(data)
+    async function handleSignUp({name, email, password}: FormDataProps) {
+      try {
+        const response = await api.post("/users", {name, email, password})
+        console.log(response)
+        
+        reset()
+      } catch (error) {
+        const isAppError = error instanceof AppError
+        const title = isAppError ? error.message : "Não foi possível criar a conta. Tente novamente mais tarde."
+
+        toast.show({
+          title,
+          bgColor: "red.500",
+          placement: "top",
+        })
+      }
     }
 
     function handleGoBack() {
